292eb69fc576efe02dc2d7bb9d675dfa7c979d52
Appu John <161853537+appujohn007@users.noreply.github.com>
2024-08-20 17:21:45 +0530

    Update example_folder.php

--- example_folder.php
+++ example_folder.php
@@ -2,7 +2,7 @@
 //ini_set('display_errors',true);
 require_once('src/mega.class.php');
 
-$megafile = new MEGA('https://mega.nz/#F!6ghFGDyD!uVPZiTXJTuDh_c5-cAevrw');
+$megafile = new MEGA('https://mega.nz/folder/oz8FGQiT#de8iKW7PbjMmFx6Q6ShL5Q');
 
 $megafile->download_zip(); // to download using streams. Make sure you have enough space for folder in /tmp
 ?>
